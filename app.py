9cfc9b85f2d3cf8a91caf2f1a3a35933de3c8d91
Arnav Joshi <arnavjsh12@gmail.com>
2021-05-07 11:52:17 -0400

    htlm template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import io
 import base64
 import numpy as np
@@ -103,12 +103,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     base = request.base_url
-    return  """<xmp>
-               Welcome to the Homepage of the OxygenDemandPredictor of the CovidUpchaar App
+    return render_template("homepage.html")
+    #return  """<xmp>
+     #          Welcome to the Homepage of the OxygenDemandPredictor of the CovidUpchaar App
 
-               To view the  predictive plot of Active Covid-19-cases please type /activecases after the homepage link in the url bar.
+      #         To view the  predictive plot of Active Covid-19-cases please type /activecases after the homepage link in the url bar.
               
-    #          To view the  predictive plot of Active Covid-19-cases please type /oxydemand after the homepage link in the url bar.</xmp>"""
+       #        To view the  predictive plot of Oxygen Demand till the peak in May, please type /oxydemand after the homepage link in the url bar.</xmp>"""
 
 @app.route("/activecases")
 def activeCases():
